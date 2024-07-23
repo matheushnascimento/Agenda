d845c12d08878ed19ba770231dfac74f6d6946a6
matheushnascimento <matheus.mhddn@gmail.com>
2024-07-23 12:29:03 -0300

    :rocket:add hash password proccess with `bryptjs`

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcryptjs = require("bcryptjs");
+
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
@@ -17,6 +19,8 @@ class Login {
     this.validate();
     if (this.errors.length > 0) return;
     try {
+      const salt = bcryptjs.genSaltSync();
+      this.body.password = bcryptjs.hashSync(this.body.password, salt);
       this.user = await LoginModel.create(this.body);
     } catch (e) {
       console.log(e);
